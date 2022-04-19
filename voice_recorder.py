708419fde30cbde0705f09859b48fa33ccacc814
srujith <srujith.sasi@gmail.com>
2022-04-19 13:10:52 +0530

    recorder

--- voice_recorder.py
+++ voice_recorder.py
@@ -30,70 +30,7 @@ def callback(indata, frames, time, status):
 
 
 
-def frequency_spectrum(x, sf):
-    """
-    Derive frequency spectrum of a signal from time domain
-    :param x: signal in the time domain
-    :param sf: sampling frequency
-    :returns frequencies and their content distribution
-    """
-    x = x - np.average(x)  # zero-centering
 
-    n = len(x)
-    k = arange(n)
-    tarr = n / float(sf)
-    frqarr = k / float(tarr)  # two sides frequency range
-
-    frqarr = frqarr[range(n // 2)]  # one side frequency range
-
-    x = fft(x) / n  # fft computing and normalization
-    x = x[range(n // 2)]
-
-    return frqarr, abs(x)
-
-
-# Sine sample with a frequency of 1hz and add some noise
-sr = 32  # sampling rate
-y = np.linspace(0, 2 * np.pi, sr)
-y = np.tile(np.sin(y), 5)
-y += np.random.normal(0, 1, y.shape)
-t = np.arange(len(y)) / float(sr)
-
-plt.subplot(2, 1, 1)
-plt.plot(t, y)
-plt.xlabel('t')
-plt.ylabel('y')
-
-frq, X = frequency_spectrum(y, sr)
-
-plt.subplot(2, 1, 2)
-plt.plot(frq, X, 'b')
-plt.xlabel('Freq (Hz)')
-plt.ylabel('|X(freq)|')
-plt.tight_layout()
-
-# wav sample from https://freewavesamples.com/files/Alesis-Sanctuary-QCard-Crickets.wav
-here_path = os.path.dirname(os.path.realpath(__file__))
-wav_file_name = 'trial.wav'
-wave_file_path = os.path.join(here_path, wav_file_name)
-sr, signal = wavfile.read(wave_file_path)
-
-y = signal[:, 0]  # use the first channel (or take their average, alternatively)
-t = np.arange(len(y)) / float(sr)
-
-plt.figure()
-plt.subplot(2, 1, 1)
-plt.plot(t, y)
-plt.xlabel('t')
-plt.ylabel('y')
-
-frq, X = frequency_spectrum(y, sr)
-
-plt.subplot(2, 1, 2)
-plt.plot(frq, X, 'b')
-plt.xlabel('Freq (Hz)')
-plt.ylabel('|X(freq)|')
-plt.tight_layout()
 
 
 # Functions to play, stop and record audio
@@ -119,10 +56,70 @@ def threading_rec(x):
             # Display and error if none is found
             messagebox.showerror(message="Record something to play")
     elif x == 4:
-        if file_exists:
-            plt.show()
-        else:
-            messagebox.showerror(message="record something to show frequency")
+        def frequency_spectrum(x, sf):
+            """
+            Derive frequency spectrum of a signal from time domain
+            :param x: signal in the time domain
+            :param sf: sampling frequency
+            :returns frequencies and their content distribution
+            """
+            x = x - np.average(x)  # zero-centering
+
+            n = len(x)
+            k = arange(n)
+            tarr = n / float(sf)
+            frqarr = k / float(tarr)  # two sides frequency range
+
+            frqarr = frqarr[range(n // 2)]  # one side frequency range
+
+            x = fft(x) / n  # fft computing and normalization
+            x = x[range(n // 2)]
+
+            return frqarr, abs(x)
+
+        # Sine sample with a frequency of 1hz and add some noise
+        sr = 32  # sampling rate
+        y = np.linspace(0, 2 * np.pi, sr)
+        y = np.tile(np.sin(y), 5)
+        y += np.random.normal(0, 1, y.shape)
+        t = np.arange(len(y)) / float(sr)
+
+        plt.subplot(2, 1, 1)
+        plt.plot(t, y)
+        plt.xlabel('t')
+        plt.ylabel('y')
+
+        frq, X = frequency_spectrum(y, sr)
+
+        plt.subplot(2, 1, 2)
+        plt.plot(frq, X, 'b')
+        plt.xlabel('Freq (Hz)')
+        plt.ylabel('|X(freq)|')
+        plt.tight_layout()
+
+        # wav sample from https://freewavesamples.com/files/Alesis-Sanctuary-QCard-Crickets.wav
+        here_path = os.path.dirname(os.path.realpath(__file__))
+        wav_file_name = 'trial.wav'
+        wave_file_path = os.path.join(here_path, wav_file_name)
+        sr, signal = wavfile.read(wave_file_path)
+
+        y = signal[:, 0]  # use the first channel (or take their average, alternatively)
+        t = np.arange(len(y)) / float(sr)
+
+        plt.figure()
+        plt.subplot(2, 1, 1)
+        plt.plot(t, y)
+        plt.xlabel('t')
+        plt.ylabel('y')
+
+        frq, X = frequency_spectrum(y, sr)
+
+        plt.subplot(2, 1, 2)
+        plt.plot(frq, X, 'b')
+        plt.xlabel('Freq (Hz)')
+        plt.ylabel('|X(freq)|')
+        plt.tight_layout()
+        plt.show()
 
 
 # Recording function
